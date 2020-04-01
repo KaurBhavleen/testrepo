242e062f9190d44a973cb05f5726ac6960bfcd16
KaurBhavleen <62898525+KaurBhavleen@users.noreply.github.com>
2020-04-01 19:06:22 +0530

    Update pipeline_script.groovy

--- Jenkins/pipeline_script.groovy
+++ Jenkins/pipeline_script.groovy
@@ -1,4 +1,4 @@
-def staticStackName = "versapipelinetesting"
+def staticStackName = "testpipeline"
 
 pipeline  {
    agent any
@@ -6,17 +6,10 @@ pipeline  {
       stage('Read Test Inputs from CSV File') {
          steps {
             echo "Configuration"
-
-            checkout([$class: 'GitSCM',
-                branches: [[name: '*/master']],
-                doGenerateSubmoduleConfigurations: false,
-                extensions: [[$class: 'CleanCheckout']],
-                submoduleCfg: [],
-                userRemoteConfigs: [[credentialsId: '82f6da67-14bb-4091-84fb-05ec00d7a2d8', url: 'https://github.com/KaurBhavleen/testrepo.git']]
-            ])
+        
             script {
               def counter=0
-              readFile("/var/lib/jenkins/workspace/Test_Pipeline2/pipeline-inputs.csv").split('\n').each { line, count ->
+              readFile("https://github.com/KaurBhavleen/testrepo.git/Jenkins/pipeline-inputs.csv").split('\n').each { line, count ->
                 def fields = line.split(',')
                 counter = counter + 1
                 for(String item: fields) {
